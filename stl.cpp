ea83509470cefc4158c3a00e091c781917986948
nehanlkt <nehanlkt@gmail.com>
2026-02-04 15:31:46 +0530

    standard template libraries

--- stl.cpp
+++ stl.cpp
@@ -12,13 +12,16 @@ void pairs()
     cout<<arr[1].first;
 }
 void vectors(){
-    
+    vector<int> v;
+    v.push_back(1);
+    v.emplace_back(2);
 }
 int main()
 {
     freopen("input.txt", "r", stdin);
     freopen("ans.txt", "w", stdout);
 
-        pairs();
+        // pairs();
+        vectors();
     }
 
