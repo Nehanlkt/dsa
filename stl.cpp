398c943e1cd01309eaa51b36b43e2ede25b00b63
nehanlkt <nehanlkt@gmail.com>
2026-02-04 15:28:32 +0530

    standard template libraries

---
+++ stl.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+void pairs()
+{
+    pair<int, int> a = {1, 2};
+    cout << a.first << " " << a.second;
+    cout<<endl;
+    pair<int,pair<int,int>> b={1,{2,4}};
+    cout<<b.first<<" "<<b.second.first<<b.second.second;
+    cout<<endl;
+    pair<int,int> arr[]={{1,2},{1,3},{1,4}};
+    cout<<arr[1].first;
+}
+void vectors(){
+    
+}
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("ans.txt", "w", stdout);
+
+        pairs();
+    }
+
