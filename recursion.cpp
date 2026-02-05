8bfe2e81104300cbae4aa058c33b5806da53a640
nehanlkt <nehanlkt@gmail.com>
2026-02-05 18:45:52 +0530

    recursion

--- recursion.cpp
+++ recursion.cpp
@@ -1,6 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
+void f(int i,int N){
+    if(i>N)return;
+    cout<<"Neha"<<endl;
+    f(i+1,N);
+    // if endln given here prints simply spaces 5 n neha5times in same line only
+}
 
+void f1(int i,int N){
+    if(i>N)return;
+    cout<<i<<endl;
+    f1(i+1,N);
+
+}
+
+void f2(int i,int N){
+      if(i<1)return;
+      cout<<(i)<<endl;
+      f2(i-1,N);
+}
+
+void f3(int i,int N){
+    // backtracting
+    if(i<1)return;
+    f3(i-1,N);
+    cout<<i;
+}
 
 
 
@@ -15,5 +40,9 @@ int main()
     {
         int N;
         cin >> N;
+        // f(1,N);
+        // f2(N,N);
+        f3(N,N);
 
     }
+}
