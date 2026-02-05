778c9aecb116d87a7e4e8270209869aa15112b13
nehanlkt <nehanlkt@gmail.com>
2026-02-05 17:54:46 +0530

    numbers

---
+++ recursion.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("anss.txt", "w", stdout);
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int N;
+        cin >> N;
+
+    }
