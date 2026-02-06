344064fdd21124d0a2d61aaa4fbac7504df895bd
nehanlkt <nehanlkt@gmail.com>
2026-02-06 08:21:23 +0530

    hashing

---
+++ hashing.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    freopen("input.txt", "r", stdin);
+    freopen("anss.txt", "w", stdout);
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    // precompilation
+    int hash[1000001]={0};
+    for(int i=0;i<n;i++){
+        hash[arr[i]]+=1;
+    }
+    // fetch
+    int q;
+    cin>>q;
+    while(q--){
+        int num;
+        cin>>num;
+        cout<<hash[num]<<endl;
+    }
+    return 0;
+
+
+}
