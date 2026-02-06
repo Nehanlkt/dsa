d232ae419e6eb933784cb54fe366714bae750084
nehanlkt <nehanlkt@gmail.com>
2026-02-06 13:51:57 +0530

    hashing

--- hashing.cpp
+++ hashing.cpp
@@ -27,24 +27,51 @@ int main(){
 
 
 
-    string s;
-    cin>>s;
+    // string s;
+    // cin>>s;
+    // // precompilation
+    // int hash[26]={0};
+    // for(int i=0;i<s.size();i++){
+    //     hash[s[i]-'a']+=1;
+    // }
+    // // fetch
+    // int q;
+    // cin>>q;
+    // while(q--){
+    //     char ch;
+    //     cin>>ch;
+    //     cout<<hash[ch-'a']<<endl;
+    // }
+    // return 0;
+    
+    
+
+    //hashing using map
+    int n;
+    cin>>n;
+    int arr[n];
+    map<int,int> m;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+        m[arr[i]]+=1;
+    }
     // precompilation
-    int hash[26]={0};
-    for(int i=0;i<s.size();i++){
-        hash[s[i]-'a']+=1;
+    int hash[13]={0};
+    for(int i=0;i<n;i++){
+        hash[arr[i]]+=1;
     }
     // fetch
     int q;
     cin>>q;
     while(q--){
-        char ch;
-        cin>>ch;
-        cout<<hash[ch-'a']<<endl;
+        int num;
+        cin>>num;
+        cout<<m[num]<<endl;
     }
     return 0;
 
-    
+
     
 }
 
