26d51998b6f4a2885266aaa44e1858b44a7293ec
nehanlkt <nehanlkt@gmail.com>
2026-02-06 12:12:42 +0530

    recursion

--- hashing.cpp
+++ hashing.cpp
@@ -3,27 +3,32 @@ using namespace std;
 int main(){
     freopen("input.txt", "r", stdin);
     freopen("anss.txt", "w", stdout);
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++)
-    {
-        cin>>arr[i];
-    }
-    // precompilation
-    int hash[13]={0};
-    for(int i=0;i<n;i++){
-        hash[arr[i]]+=1;
-    }
-    // fetch
-    int q;
-    cin>>q;
-    while(q--){
-        int num;
-        cin>>num;
-        cout<<hash[num]<<endl;
-    }
-    return 0;
+    // int n;
+    // cin>>n;
+    // int arr[n];
+    // for(int i=0;i<n;i++)
+    // {
+    //     cin>>arr[i];
+    // }
+    // // precompilation
+    // int hash[13]={0};
+    // for(int i=0;i<n;i++){
+    //     hash[arr[i]]+=1;
+    // }
+    // // fetch
+    // int q;
+    // cin>>q;
+    // while(q--){
+    //     int num;
+    //     cin>>num;
+    //     cout<<hash[num]<<endl;
+    // }
+    // return 0;
+
+
+
+    
 
 
 }
+
